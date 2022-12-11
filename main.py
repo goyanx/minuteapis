737af6fa4aaacf422e5fb075b5b366db5af1cdd5
Justin <gonx1@yahoo.com>
2022-12-11 19:06:22 +0800

    update

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+import logging
+import uvicorn
 
 app = FastAPI()
 
@@ -6,3 +8,11 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/api")
+async def get_analysis(p: str = 'Peter Sullivan is an associate in the Risk Assessment and Management Office at MBS'):
+    return {"message": "Hello World"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, debug=True)
