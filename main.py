2e3b67f355dcc7a81021d7145dfadc71e8a3b917
Justin <gonx1@yahoo.com>
2022-12-11 20:46:50 +0800

    used pip freeze > requirements.txt

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI, Request
 import logging
 import uvicorn
+from allennlp.predictors.predictor import Predictor
+import allennlp_models.rc
 
 app = FastAPI()
+
+
+@app.on_event("startup")
+def load_model():
+    global predictor
+    predictor = Predictor.from_path(
+        "https://storage.googleapis.com/allennlp-public-models/bidaf-elmo.2021-02-11.tar.gz")
 
 
 @app.get("/")
@@ -11,8 +20,9 @@ async def root():
 
 
 @app.get("/api")
-async def get_analysis(p: str = 'Peter Sullivan is an associate in the Risk Assessment and Management Office at MBS'):
-    return {"message": "Hello World"}
+async def get_analysis(p: str = 'Peter Sullivan is an associate in the Risk Assessment and Management Office at MBS', q: str = 'what is the problem ?'):
+    results = predictor.predict(passage=p, question=q)
+    return {"answer": results}
 
 if __name__ == "__main__":
     uvicorn.run(app, debug=True)
